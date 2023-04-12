7f1f11ed0d6f2d0337156ec4a56ab3312683a38b
GuilhermeArsenio <78441164+GuilhermeArsenio@users.noreply.github.com>
2023-04-12 15:44:39 +0100

    init

---
+++ .eslintrc.js
@@ -0,0 +1,16 @@
+// eslint-disable-next-line no-undef
+module.exports = {
+  root: true,
+  env: {
+    node: true,
+    es2020: true,
+  },
+  extends: "eslint:recommended",
+  parserOptions: {
+    ecmaVersion: 12,
+    sourceType: "module",
+  },
+  rules: {
+    semi: [2, "always"],
+  },
+};
